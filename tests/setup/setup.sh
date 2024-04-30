38e1f3b3bd21ae6e48eecef7dd1227f9410d690b
Wiktor Maj <wiktor.maj@suse.com>
2024-04-30 12:36:47 +0200

    FINISHING: setup done

--- tests/setup/setup.sh
+++ tests/setup/setup.sh
@@ -9,61 +9,53 @@ set_network() {
     mysql-network
 }
 
-set_nodes() {
-  docker build \
-    --tag node \
-    ./setup
-
+set_ssh_keys() {
   ssh-keygen -t rsa -b 2048 -f id_rsa -N ""
-  sleep 5
+  chmod 600 id_rsa.pub
   ssh-add id_rsa
   # echo "StrictHostKeyChecking no" | sudo tee --append "/etc/ssh/ssh_config"
+}
+
+set_hostname_resolution() {
+  local current_node="${1}"
+  local node=0
+  while [ ${node} -lt ${nodes} ]; do
+    if [ ${node} != ${current_node} ]; then
+      docker exec \
+        --user "root" \
+        "node-${current_node}" \
+          sh -c "echo '172.18.0.$((2 + ${node}))  node-${node}' >> /etc/hosts"
+    fi
+    let "node+=1"
+  done
+}
+
+set_nodes() {
+  docker build \
+    --tag "node" \
+    ./setup
 
   for ((node = 0; node < ${nodes}; node++)); do
-    if [ $node == 4 ]; then
-      docker run \
+    docker run \
       --detach \
       --privileged \
-      --ulimit nofile=65535 \
+      --ulimit "nofile=65535" \
       --name "node-${node}" \
       --hostname "node-${node}" \
-      --publish "$((2222+${node})):22" \
-      --publish "8088:8088" \
-      --ip 172.18.0.$((2 + $node)) \
-      --network mysql-network \
+      --ip "172.18.0.$((2 + ${node}))" \
+      --network "mysql-network" \
       node
-    else
-      docker run \
-        --detach \
-        --privileged \
-        --ulimit nofile=65535 \
-        --name "node-${node}" \
-        --hostname "node-${node}" \
-        --publish "$((2222+${node})):22" \
-        --publish "$((2378+${node})):2377" \
-        --publish "$((7947+${node})):7946" \
-        --publish "$((4790+${node})):4789" \
-        --ip 172.18.0.$((2 + $node)) \
-        --network mysql-network \
-        node
-    fi
-
-    docker cp id_rsa.pub "node-${node}":/root/.ssh
 
     docker exec \
+      --interactive \
+      --user "root" \
       "node-${node}" \
-        sh -c "cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys"
+        sh -c "cat > /root/.ssh/authorized_keys" < id_rsa.pub
 
-    for ((n = 0; n < ${nodes}; n++)); do
-      if [ $n != $node ]; then
-        docker exec \
-          --user root \
-          "node-${node}" \
-            sh -c "echo '172.18.0.$((2 + n))  node-${n}' >> /etc/hosts"
-      fi
-    done
+    set_hostname_resolution "${node}"
   done
 }
 
 set_network
+set_ssh_keys
 set_nodes
