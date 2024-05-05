8f52a99023b39494e01811981575fa32eaa96b0c
Wiktor Maj <wiktor.maj@suse.com>
2024-05-05 15:12:31 +0200

    code good to go

--- tests/setup/variables.tf
+++ tests/setup/variables.tf
@@ -4,5 +4,26 @@ variable "nodes" {
   default     = 5
 }
 
-# docker network subnet
-# docker network gateway
+variable "node_version" {
+  description = "Version of the container nodes deployed"
+  type        = string
+  default     = "1.0"
+}
+
+variable "node_prefix" {
+  description = "Prefix of the container nodes"
+  type        = string
+  default     = "node"
+}
+
+variable "subnet" {
+  description = "Network subnet for the container nodes"
+  type        = string
+  default     = "172.18.0.0/16"
+}
+
+variable "gateway" {
+  description = "Network gateway for the container nodes"
+  type        = string
+  default     = "172.18.0.1"
+}
