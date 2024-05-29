2558bbd4488ba9e804bab0e7319ee6f692f3d634
Wiktor Maj <wiktor.maj@suse.com>
2024-05-29 19:29:21 +0200

    functional checkpoint, redis celery working

--- tests/testsuite/roles/testing/files/functional.py
+++ tests/testsuite/roles/testing/files/functional.py
@@ -1,6 +1,7 @@
 import json
 import re
 
+import ast
 import redis
 import docker
 import requests
@@ -8,17 +9,14 @@ import requests
 # redis_host = '<redis_host>'
 # redis_port = <redis_port>
 
-# try:
-#     redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-#     redis_client.ping()
-#     print("Connection to Redis successful.")
-# except Exception as e:
-#     print(f"Error connecting to Redis: {e}")
-
 MYSQL_ROOT_PASSWORD='mysql'
 NODE_PREFIX='node'
 NODES=5
 
+REDIS_HOSTNAME='redis'
+REDIS_PORT=6379
+
+CELERY_SQL_LAB_TASK_ANNOTATIONS='sql_lab.get_sql_results'
 
 class BaseContainerConnection:
     def __init__(self) -> None:
@@ -37,14 +35,65 @@ class BaseContainerConnection:
         if output.find(text) != -1:
             return True
         return False
-
-    @staticmethod
-    def find_key_value_from_bytes():
-        pass
     
     @staticmethod
     def extract_session_cookie(request_output: str) -> str:
         return re.search(r'Set-Cookie: session=(.*?);', request_output).group(1)
+    
+    @staticmethod
+    def decode_command_output(command: bytes) -> dict:
+        try:
+            return ast.literal_eval(command.decode('utf-8'))
+        except ValueError:
+            return json.loads(command.decode('utf-8'))
+
+
+class Redis(BaseContainerConnection):
+    def __init__(self) -> None:
+        super().__init__()
+        self.node = 'node-4'
+
+    def status(self) -> bool | ConnectionError:
+        test_connection: bytes = self.run_command_on_the_container(f"docker exec {self.get_container_name()} python3 -c 'import redis; print(redis.StrictRedis(host=\"{REDIS_HOSTNAME}\", port={REDIS_PORT}).ping())'")
+        if self.find_in_the_output(test_connection, b'True'):
+            return True
+        raise ConnectionError('asd')
+
+    def fetch_query_result(self, results_key: float):
+        query_result: bytes = self.run_command_on_the_container(f"docker exec {self.get_container_name()} python3 -c 'import redis; print(redis.StrictRedis(host=\"{REDIS_HOSTNAME}\", port={REDIS_PORT}).get(\"{results_key}\"))'")
+        if self.find_in_the_output(query_result, b"None"):
+            raise Exception
+        return True
+
+class Celery(BaseContainerConnection):
+    def __init__(self) -> None:
+        super().__init__()
+        self.broker: str = f"redis://{REDIS_HOSTNAME}:{REDIS_PORT}/0"
+        self.node = 'node-4'
+    
+    def status(self) -> bool | ConnectionError:
+        test_connection: bytes = self.run_command_on_the_container(f"docker exec {self.get_container_name()} python3 -c 'import celery; print(celery.Celery(\"tasks\", broker=\"{self.broker}\").control.inspect().ping())'")
+        if self.find_in_the_output(test_connection, b"{'ok': 'pong'}"):
+            return True
+        raise ConnectionError('asd')
+
+    def find_processed_queries(self):
+        celery_workers_stats: dict = self.decode_command_output(
+            self.run_command_on_the_container(f"docker exec {self.get_container_name()} python3 -c 'import celery; print(celery.Celery(\"tasks\", broker=\"{self.broker}\").control.inspect().stats())'")
+        )
+        celery_worker_id: str = next(iter(celery_workers_stats))
+        if celery_workers_stats[celery_worker_id]['total'][CELERY_SQL_LAB_TASK_ANNOTATIONS] == 0:
+            raise Exception
+        return True
+
+    def check_cache(self):
+        celery_workers_configuration: dict = self.decode_command_output(
+            self.run_command_on_the_container(f"docker exec {self.get_container_name()} python3 -c 'import celery; print(celery.Celery(\"tasks\", broker=\"{self.broker}\").control.inspect().conf())'")
+        )
+        celery_worker_id: str = next(iter(celery_workers_configuration))
+        if all(features in celery_workers_configuration[celery_worker_id]['include'] for features in ['superset.tasks.cache', 'superset.tasks.scheduler']):
+            return True    
+        raise Exception
 
 class SupersetNodeFunctionalTests(BaseContainerConnection):
     def __init__(self, node: str) -> None:
@@ -52,9 +101,13 @@ class SupersetNodeFunctionalTests(BaseContainerConnection):
         self.node: str = node
         self._api_default_url: str = "http://localhost:8088/api/v1"
         self._api_authorization_header: str = f"Authorization: Bearer {self._login_to_superset_api()}"
+        
         a = self._login_to_superset()
         self._api_csrf_header: str = f"X-CSRFToken: {a['csrf_token']}"
         self._api_session_header: str = f"Cookie: session={a['session_token']}"
+
+        self.redis = Redis()
+        self.celery = Celery()
 
     def _login_to_superset_api(self):
         headers: str = "Content-Type: application/json"
@@ -79,31 +132,36 @@ class SupersetNodeFunctionalTests(BaseContainerConnection):
         # self.run_command_on_the_container(f"docker exec {self.get_container_name()} superset load_examples")
         dashboard_charts: bytes = self.run_command_on_the_container(f"curl --silent --url {self._api_default_url}/dashboard/1 --header '{self._api_authorization_header}'")
         dashboard_datasets: bytes = self.run_command_on_the_container(f"curl --silent --url {self._api_default_url}/dashboard/1/datasets --header '{self._api_authorization_header}'")
-        print(json.loads(dashboard_charts.decode('utf-8')).get("result")["charts"])
-        print(json.loads(dashboard_datasets.decode('utf-8')).get("result"))
         assert json.loads(dashboard_charts.decode('utf-8')).get("result")["charts"] != [], 'err'
         assert json.loads(dashboard_datasets.decode('utf-8')).get("result") != [], 'err'
 
-    def database_status(self):
+    def database_status(self) -> bool | ConnectionError:
         payload: str = '{"database_name": "MySQL","sqlalchemy_uri": "mysql+mysqlconnector://root:mysql@172.18.0.2:6446/superset","impersonate_user": false}'
         test_database_connection: bytes = self.run_command_on_the_container(f"curl --silent http://localhost:8088/api/v1/database/test_connection/ --header '{self._api_authorization_header}' --header '{self._api_csrf_header}' --header '{self._api_session_header}' --header 'Content-Type: application/json' --data '{payload}'")
-        print(test_database_connection)
-        self.find_in_the_output(test_database_connection, b'{"message":"OK"}'), 'edw'
-        # csrf = self.csrf()
-        # print(self._api_authorization_header)
-        # print(csrf)
-        # command = f"""curl --url {self._api_default_url}/database/test_connection/ -H '{self._api_authorization_header}' -H 'X-CSRFToken: {csrf}' -H 'Content-Type: application/json' --data '{payload}'"""
-        # print(command)
-        # test_database_connection: bytes = self.run_command_on_the_container(command)
-        # print(test_database_connection)
-        # t = self.run_command_on_the_container("""curl 'http://localhost:8088/api/v1/sqllab/execute/'""" +  f""" -H '{self._api_authorization_header}' -H 'X-CSRFToken: {csrf}'""" + """ -H 'Accept: application/json' -H 'Accept-Language: en-US,en' -H 'Connection: keep-alive' -H 'Content-Type: application/json' -H 'Origin: http://localhost:8092' -H 'Referer: http://localhost:8092/sqllab/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: same-origin' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-GPC: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' -H 'sec-ch-ua: "Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Linux"' --data '{"database_id":2,"runAsync":true,"sql":"SELECT * FROM superset.logs;"}'""")
-        # print(t)
+        if self.find_in_the_output(test_database_connection, b'{"message":"OK"}'):
+            return True
+        raise ConnectionError('asd')
 
-    def redis_status(self):
-        pass
-
-    def celery_status(self):
-        pass
+    def run_query(self) -> float | Exception:
+        if self.database_status() and self.redis.status() and self.celery.status():
+            payload: str = '{"database_id":2,"runAsync":true,"sql":"SELECT * FROM superset.logs;"}'
+            sqllab_run_query: bytes = self.run_command_on_the_container(f"curl --silent http://localhost:8088/api/v1/sqllab/execute/ --header 'Content-Type: application/json' --header '{self._api_session_header}' --header '{self._api_csrf_header}' --data '{payload}'")
+            dttm_time_query_identifier: float = json.loads(sqllab_run_query.decode('utf-8')).get("query").get("startDttm")
+            return dttm_time_query_identifier
+        raise Exception
+    
+    def get_query_results(self, dttm_time_query_identifier: float):        
+        import time
+        time.sleep(1)
+        query_result: dict = self.decode_command_output(
+            self.run_command_on_the_container(f"curl --silent 'http://localhost:8088/api/v1/query/updated_since?q=(last_updated_ms:{dttm_time_query_identifier})' --header 'Accept: application/json' --header '{self._api_session_header}' --header '{self._api_csrf_header}'")
+        )
+        if query_result.get("result")[0]['state'] == 'success':
+            results_key: str = f"superset_results{query_result.get('result')[0]['resultsKey']}"
+            if self.redis.fetch_query_result(results_key) and self.celery.find_processed_queries() and self.celery.check_cache():
+                return True
+            raise Exception('vef')
+        raise Exception('adwedde')
 
     def swarm_status(self):
         swarm_status_output: bytes = self.run_command_on_the_container("docker info")
@@ -132,7 +190,9 @@ class MgmtNodeFunctionalTests(BaseContainerConnection):
 
 s = SupersetNodeFunctionalTests(f"{NODE_PREFIX}-{NODES-1}")
 # s.dashboards_status()
-s.database_status()
+# s.database_status()
+dttm = s.run_query()
+s.get_query_results(dttm)
 
 # m = MgmtNodeFunctionalTests(f"{NODE_PREFIX}-0")
 # m.cluster_status()
@@ -151,93 +211,3 @@ s.database_status()
 # print(type(a))
 # docker exec mysql-mgmt mysqlsh --json --interactive --uri root:mysql@node-0:6446 --execute "dba.getCluster('cluster').status();"
 
-# curl 'http://localhost:8088/api/v1/database/test_connection/' \
-#   -H 'Content-Type: application/json' \
-#   -H 'Cookie: session=.eJwljktOBDEMRO-SNQvHdpx4LtNybEcgRozUPbNC3J0glvV5pfouxzrzei-35_nKt3J8RLkVC6Hejdu0UdeSgatHtEXGizABe-A00nCvqsShPKtwS4BJlQRSl-5E0DyCHUdnSO8YbD27KZKioqVB1d2YEzJk-fRAAomyj7yuPP_f1C39OtfxfHzm1zYUmjQezcUNAGonZhOmlATmcBVtw-Jv5v5wu-dmNvjzC8DmQ6Q.Zk3OVw.XgpKZ1GVYHg-3cNmDfA4-IsDC5M' \
-#   -H 'X-CSRFToken: IjkwNTY1NDg1YzZjYTAwMDE3MzQ0YTY0M2U2ZTA0NGRjOTY5NThhZGQi.Zk3OVw.NtQU58eUFRfBeehwqNqJfPO_np4' \
-#   --data '{"sqlalchemy_uri":"mysql+mysqlconnector://root:mysql@172.18.0.2:6446/superset","database_name":"MySQL","masked_encrypted_extra":""}
-
-# curl 'http://localhost:8092/api/v1/sqllab/execute/' \
-#   -H 'Accept: application/json' \
-#   -H 'Accept-Language: en-US,en' \
-#   -H 'Connection: keep-alive' \
-#   -H 'Content-Type: application/json' \
-#   -H 'Cookie: session=.eJwljktOBDEMRO-SNQvHdpx4LtNybEcgRozUPbNC3J0glvV5pfouxzrzei-35_nKt3J8RLkVC6Hejdu0UdeSgatHtEXGizABe-A00nCvqsShPKtwS4BJlQRSl-5E0DyCHUdnSO8YbD27KZKioqVB1d2YEzJk-fRAAomyj7yuPP_f1C39OtfxfHzm1zYUmjQezcUNAGonZhOmlATmcBVtw-Jv5v5wu-dmNvjzC8DmQ6Q.Zk3OVw.XgpKZ1GVYHg-3cNmDfA4-IsDC5M' \
-#   -H 'Origin: http://localhost:8092' \
-#   -H 'Referer: http://localhost:8092/sqllab/' \
-#   -H 'Sec-Fetch-Dest: empty' \
-#   -H 'Sec-Fetch-Mode: same-origin' \
-#   -H 'Sec-Fetch-Site: same-origin' \
-#   -H 'Sec-GPC: 1' \
-#   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' \
-#   -H 'X-CSRFToken: IjkwNTY1NDg1YzZjYTAwMDE3MzQ0YTY0M2U2ZTA0NGRjOTY5NThhZGQi.Zk3OVw.NtQU58eUFRfBeehwqNqJfPO_np4' \
-#   -H 'sec-ch-ua: "Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' \
-#   -H 'sec-ch-ua-mobile: ?0' \
-#   -H 'sec-ch-ua-platform: "Linux"' \
-#   --data '{"database_id":2,"runAsync":true,"sql":"SELECT * FROM superset.logs;"}'
-#   --data-raw '{"client_id":"iPD2lRZkN","database_id":2,"json":true,"runAsync":true,"schema":"superset","sql":"SELECT * FROM superset.logs;\n","sql_editor_id":"1","tab":"Untitled Query 1","tmp_table_name":"","select_as_cta":false,"ctas_method":"TABLE","queryLimit":1000,"expand_data":true}' ;
-
-# curl 'http://localhost:8092/api/v1/query/updated_since?q=(last_updated_ms:1716381375000)' \
-#   -H 'Accept: application/json' \
-#   -H 'Accept-Language: en-US,en' \
-#   -H 'Connection: keep-alive' \
-#   -H 'Cookie: session=.eJwljktOBDEMRO-SNQvHdpx4LtNybEcgRozUPbNC3J0glvV5pfouxzrzei-35_nKt3J8RLkVC6Hejdu0UdeSgatHtEXGizABe-A00nCvqsShPKtwS4BJlQRSl-5E0DyCHUdnSO8YbD27KZKioqVB1d2YEzJk-fRAAomyj7yuPP_f1C39OtfxfHzm1zYUmjQezcUNAGonZhOmlATmcBVtw-Jv5v5wu-dmNvjzC8DmQ6Q.Zk3OVw.XgpKZ1GVYHg-3cNmDfA4-IsDC5M' \
-#   -H 'Referer: http://localhost:8092/sqllab/' \
-#   -H 'Sec-Fetch-Dest: empty' \
-#   -H 'Sec-Fetch-Mode: same-origin' \
-#   -H 'Sec-Fetch-Site: same-origin' \
-#   -H 'Sec-GPC: 1' \
-#   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' \
-#   -H 'X-CSRFToken: IjkwNTY1NDg1YzZjYTAwMDE3MzQ0YTY0M2U2ZTA0NGRjOTY5NThhZGQi.Zk3OVw.NtQU58eUFRfBeehwqNqJfPO_np4' \
-#   -H 'sec-ch-ua: "Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' \
-#   -H 'sec-ch-ua-mobile: ?0' \
-#   -H 'sec-ch-ua-platform: "Linux"' ;
-# curl 'http://localhost:8092/api/v1/query/updated_since?q=(last_updated_ms:1716381785000)' \
-#   -H 'Accept: application/json' \
-#   -H 'Accept-Language: en-US,en' \
-#   -H 'Connection: keep-alive' \
-#   -H 'Cookie: session=.eJwljktOBDEMRO-SNQvHdpx4LtNybEcgRozUPbNC3J0glvV5pfouxzrzei-35_nKt3J8RLkVC6Hejdu0UdeSgatHtEXGizABe-A00nCvqsShPKtwS4BJlQRSl-5E0DyCHUdnSO8YbD27KZKioqVB1d2YEzJk-fRAAomyj7yuPP_f1C39OtfxfHzm1zYUmjQezcUNAGonZhOmlATmcBVtw-Jv5v5wu-dmNvjzC8DmQ6Q.Zk3OVw.XgpKZ1GVYHg-3cNmDfA4-IsDC5M' \
-#   -H 'Referer: http://localhost:8092/sqllab/' \
-#   -H 'Sec-Fetch-Dest: empty' \
-#   -H 'Sec-Fetch-Mode: same-origin' \
-#   -H 'Sec-Fetch-Site: same-origin' \
-#   -H 'Sec-GPC: 1' \
-#   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' \
-#   -H 'X-CSRFToken: IjkwNTY1NDg1YzZjYTAwMDE3MzQ0YTY0M2U2ZTA0NGRjOTY5NThhZGQi.Zk3OVw.NtQU58eUFRfBeehwqNqJfPO_np4' \
-#   -H 'sec-ch-ua: "Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' \
-#   -H 'sec-ch-ua-mobile: ?0' \
-#   -H 'sec-ch-ua-platform: "Linux"' ;
-# curl 'http://localhost:8092/api/v1/sqllab/results/?q=(key:%27551d6b3c-e4d3-4fdc-99bc-5f511a08bdd9%27,rows:10000)' \
-#   -H 'Accept: application/json' \
-#   -H 'Accept-Language: en-US,en' \
-#   -H 'Connection: keep-alive' \
-#   -H 'Cookie: session=.eJwljktOBDEMRO-SNQvHdpx4LtNybEcgRozUPbNC3J0glvV5pfouxzrzei-35_nKt3J8RLkVC6Hejdu0UdeSgatHtEXGizABe-A00nCvqsShPKtwS4BJlQRSl-5E0DyCHUdnSO8YbD27KZKioqVB1d2YEzJk-fRAAomyj7yuPP_f1C39OtfxfHzm1zYUmjQezcUNAGonZhOmlATmcBVtw-Jv5v5wu-dmNvjzC8DmQ6Q.Zk3OVw.XgpKZ1GVYHg-3cNmDfA4-IsDC5M' \
-#   -H 'Referer: http://localhost:8092/sqllab/' \
-#   -H 'Sec-Fetch-Dest: empty' \
-#   -H 'Sec-Fetch-Mode: same-origin' \
-#   -H 'Sec-Fetch-Site: same-origin' \
-#   -H 'Sec-GPC: 1' \
-#   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' \
-#   -H 'X-CSRFToken: IjkwNTY1NDg1YzZjYTAwMDE3MzQ0YTY0M2U2ZTA0NGRjOTY5NThhZGQi.Zk3OVw.NtQU58eUFRfBeehwqNqJfPO_np4' \
-#   -H 'sec-ch-ua: "Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' \
-#   -H 'sec-ch-ua-mobile: ?0' \
-#   -H 'sec-ch-ua-platform: "Linux"' ;
-# curl 'http://localhost:8092/tabstateview/1' \
-#   -X 'PUT' \
-#   -H 'Accept: application/json' \
-#   -H 'Accept-Language: en-US,en' \
-#   -H 'Connection: keep-alive' \
-#   -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryAOAQTIbdal4tjhlW' \
-#   -H 'Cookie: session=.eJwljktOBDEMRO-SNQvHdpx4LtNybEcgRozUPbNC3J0glvV5pfouxzrzei-35_nKt3J8RLkVC6Hejdu0UdeSgatHtEXGizABe-A00nCvqsShPKtwS4BJlQRSl-5E0DyCHUdnSO8YbD27KZKioqVB1d2YEzJk-fRAAomyj7yuPP_f1C39OtfxfHzm1zYUmjQezcUNAGonZhOmlATmcBVtw-Jv5v5wu-dmNvjzC8DmQ6Q.Zk3OVw.XgpKZ1GVYHg-3cNmDfA4-IsDC5M' \
-#   -H 'Origin: http://localhost:8092' \
-#   -H 'Referer: http://localhost:8092/sqllab/' \
-#   -H 'Sec-Fetch-Dest: empty' \
-#   -H 'Sec-Fetch-Mode: same-origin' \
-#   -H 'Sec-Fetch-Site: same-origin' \
-#   -H 'Sec-GPC: 1' \
-#   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36' \
-#   -H 'X-CSRFToken: IjkwNTY1NDg1YzZjYTAwMDE3MzQ0YTY0M2U2ZTA0NGRjOTY5NThhZGQi.Zk3OVw.NtQU58eUFRfBeehwqNqJfPO_np4' \
-#   -H 'sec-ch-ua: "Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"' \
-#   -H 'sec-ch-ua-mobile: ?0' \
-#   -H 'sec-ch-ua-platform: "Linux"' \
-#   --data-raw $'------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="database_id"\r\n\r\n2\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="schema"\r\n\r\n"superset"\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="sql"\r\n\r\n"SELECT * FROM superset.logs;\\n"\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="label"\r\n\r\n"Untitled Query 1"\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="query_limit"\r\n\r\n1000\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="latest_query_id"\r\n\r\n"iPD2lRZkN"\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="hide_left_bar"\r\n\r\nfalse\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="autorun"\r\n\r\nfalse\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW\r\nContent-Disposition: form-data; name="extra_json"\r\n\r\n"{\\"updatedAt\\":1716381789266,\\"version\\":1}"\r\n------WebKitFormBoundaryAOAQTIbdal4tjhlW--\r\n'
