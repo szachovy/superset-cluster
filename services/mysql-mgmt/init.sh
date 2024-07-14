38ac2c39a6cebd05a40d9bc3e70610017bc54af1
Wiktor Maj <wiktor.maj@suse.com>
2024-07-14 16:37:28 +0200

    remove redundant export

--- services/mysql-mgmt/init.sh
+++ services/mysql-mgmt/init.sh
@@ -9,10 +9,3 @@ export SECONDARY_SECOND_MYSQL_NODE="${6}"
 
 cd /opt/superset-cluster/mysql-mgmt
 docker compose up initcontainer && docker compose up maincontainer --detach
-
-export IS_PRIMARY_MGMT_NODE="true"
-export VIRTUAL_IP_ADDRESS="172.18.0.8"
-export NETWORK_INTERFACE="eth0"
-export PRIMARY_MYSQL_NODE="node-1"
-export SECONDARY_FIRST_MYSQL_NODE="node-2"
-export SECONDARY_SECOND_MYSQL_NODE="node-3"
