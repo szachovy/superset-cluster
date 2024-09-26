5899125f5efa033b376f61c6915de4fa1057a0c4
szachovy <wiktor.maj@suse.com>
2024-09-26 13:22:28 +0200

    small refactors

--- services/superset/superset_config.py
+++ services/superset/superset_config.py
@@ -3,12 +3,12 @@ import flask_caching.backends.rediscache
 import os
 
 class CeleryConfig(object):
-    broker_url = f"redis://redis:6379/0"
+    broker_url = "redis://redis:6379/0"
     imports = (
         "superset.sql_lab",
         "superset.tasks.scheduler",
     )
-    result_backend = f"redis://redis:6379/0"
+    result_backend = "redis://redis:6379/0"
     worker_prefetch_multiplier = 10
     task_acks_late = True
     task_annotations = {
@@ -22,10 +22,10 @@ with open('/run/secrets/superset_secret_key', 'r') as superset_secret_key:
 
 SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://superset:cluster@{os.environ.get('VIRTUAL_IP_ADDRESS')}:6446/superset"
 CELERY_CONFIG = CeleryConfig
-RESULTS_BACKEND = flask_caching.backends.rediscache.RedisCache(host=f"redis", port=6379, key_prefix='superset_results')
+RESULTS_BACKEND = flask_caching.backends.rediscache.RedisCache(host="redis", port=6379, key_prefix='superset_results')
 FILTER_STATE_CACHE_CONFIG = {
     'CACHE_TYPE': 'RedisCache',
     'CACHE_DEFAULT_TIMEOUT': 86400,
     'CACHE_KEY_PREFIX': 'superset_filter_cache',
-    'CACHE_REDIS_URL': f"redis://redis:6379/0"
+    'CACHE_REDIS_URL': "redis://redis:6379/0"
 }
--- tests/testsuite/roles/testing/files/system.sh
+++ tests/testsuite/roles/testing/files/system.sh
@@ -10,7 +10,6 @@ VIRTUAL_NETWORK="172.18.0.0/16"  # do it in common via python
 
 mgmt_nodes=("${node_prefix}-0" "${node_prefix}-1")
 mysql_nodes=("${node_prefix}-2" "${node_prefix}-3" "${node_prefix}-4")
-# superset_node="${node_prefix}-4"
 
 _path_to_root_catalog="../.."
 
